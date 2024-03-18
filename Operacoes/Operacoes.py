d19c9533411025b507e1d45394558b0f383a60e3
Pedro <Pedrohsb04@gmail.com>
2024-03-18 11:47:43 -0300

    Adição das operações de subtração e soma com as duas imagens

--- Operacoes/Operacoes.py
+++ Operacoes/Operacoes.py
@@ -1,19 +1,40 @@
-
-import numpy as np
 import cv2
 import tkinter as tk
-from PIL import ImageOps
 from tkinter import filedialog
 
-
 # Abre a janela do explorador de arquivos
-img = filedialog.askopenfilename()
-print("Arquivo selecionado:", img)
+print('Selecione a primeira imagem: ')
 
-# testando
-for i in (1, 2, 3, 8):
-    Bitsimg = ImageOps(img, bits =i)
-    Bitsimg.show()
-    
+# Seleciona a primeira imagem
+img_path1 = filedialog.askopenfilename()
+img1 = cv2.imread(img_path1)
+img1 = cv2.resize(img1, dsize=(800, 800), interpolation=cv2.INTER_CUBIC)
+
+print("Arquivo selecionado:", img_path1)
+cv2.imshow('Imagem 1', img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print('Selecione a segunda imagem: ')
+
+# Seleciona a segunda imagem
+img_path2 = filedialog.askopenfilename()
+img2 = cv2.imread(img_path2)
+img2 = cv2.resize(img2, dsize=(800, 800), interpolation=cv2.INTER_CUBIC)
+
+print("Arquivo selecionado:", img_path2)
+cv2.imshow('Imagem 2', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+added = cv2.add(img1, img2)
+cv2.imshow("added", added)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+subtraction = cv2.subtract(img1, img2)
+cv2.imshow("subtract", subtraction)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
